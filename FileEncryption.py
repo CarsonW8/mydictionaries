cf55be473ff272ab04d4916e1ef5b94bab96f0c3
Carson Waite <Carson_Waite1@baylor.edu>
2023-09-28 15:55:06 -0500

    changes

--- FileEncryption.py
+++ FileEncryption.py
@@ -1,12 +1,32 @@
-codes = { 'A' : '%', 'a' : '9', 
+codes = {
+        'A' : '%', 'a' : '9', 
          'B' : '@', 'b' : '#',
          'C' : '-', 'c' : '!',
          'D' : '[', 'd' : ';',
-         'E' : '2', 'e' : '3'
-        #finish this dict
-         
-         
-         
+         'E' : '2', 'e' : '3',
+         'F' : '{', 'f' : '~',
+         'G' : '`', 'g' : '$',
+         'H' : 'n', 'h' : 'M',
+         'I' : '|', 'i' : '+',
+         'J' : '=', 'j' : '8',
+         'K' : '^', 'k' : '(',
+         'L' : '&', 'l' : ')',
+         'M' : '_', 'm' : '4',
+         'N' : '7', 'n' : ':',
+         'O' : '.', 'o' : '<',
+         'P' : '>', 'p' : '?',
+         'Q' : '"', 'q' : '/',
+         'R' : '5', 'r' : '6',
+         'S' : 'p', 's' : 'l',
+         'T' : 'u', 't' : 'R',
+         'U' : 'W', 'u' : 'F',
+         'V' : 'a', 'v' : 'b',
+         'W' : 'q', 'w' : 'g',
+         'X' : 'z', 'x' : 'S',
+         'Y' : 'h', 'y' : 'd',
+         'Z' : ',', 'z' : 'c',
+         ' ' : ' ', '.' : ']',
+         ':' : 'e'
          }
 
 
@@ -16,7 +36,7 @@ txt_file = security_txt.read()
 
 outfile = open('encrypted.txt', 'w')
 
-for line in txt_file.split():
+for word in txt_file.split():
     for letter in txt_file:
         outfile.write(codes[letter])
 
